a4ef329f529c1407a99b74c19f3aad6e741df2bd
Erik Andersson <dv13ean@cs.umu.se>
2019-06-05 19:15:01 +0200

    added game board generation

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 ```
-termsweeper 0.02
+termsweeper 0.03
 Usage: termsweeper [OPTION...]
-termsweeper - a simple minesweeper clone in the terminal.
+termsweeper - a simple minesweeper clone in theterminal.
 
   -h, --height=HEIGHT        Change number of rows in the game to HEIGHT.
   -m, --mines=MINES          Set the number of mines to put on the board to
-                             MINES. MINES has to be larger than 0 and smaller
+                             MINES. MINES has to be	larger than 0 and smaller
                              than HEIGHT*WIDTH.
   -w, --width=WIDTH          Change number of columns in the game to WIDTH.
   -?, --help                 Give this help list
@@ -17,12 +17,12 @@ for any corresponding short options.
 
 Report bugs to <erik_andersson007@hotmail.com>.
 ```
-README.md automatically generated on Mon 10 Oct 16:28:00 CEST 2016
+README.md automatically generated on Wed  5 Jun 19:15:35 CEST 2019
 
 last 5 commits:
 
+Wed Jun 5 19:15:01 2019 +0200 - Erik Andersson: added game board generation  
+Wed Jun 5 19:12:53 2019 +0200 - Erik Andersson: add debugging  
 Mon Oct 10 16:27:56 2016 +0200 - Erik Andersson: add drawing of border and checking of window size  
 Mon Oct 10 12:17:25 2016 +0200 - Erik Andersson: dont echo readme generation  
 Mon Oct 10 12:10:30 2016 +0200 - Erik Andersson: display settings and screen size until 'q' is pressed  
-Thu Oct 6 17:19:33 2016 +0200 - Erik Andersson: Merge branch 'master' of github.com:krokerik/termsweeper  
-Thu Oct 6 17:16:21 2016 +0200 - Erik Andersson: ncurses experiment with printing in the middle of the screen  
--- src/main.c
+++ src/main.c
@@ -4,31 +4,35 @@
 #include <ncurses.h>
 #include <math.h>
 #include <string.h>
+#include <time.h>
 
 /* Tile types */
 #define EMPTY          0
 #define MINE           1
-#define FLAG_AND_EMPTY 2
-#define FLAG_AND_MINE  3
 
-const char *argp_program_version = "termsweeper 0.02";
+/* default values */
+#define DEFAULT_W 80
+#define DEFAULT_H 40
+#define DEFAULT_M DEFAULT_H*DEFAULT_W/320
+
+const char *argp_program_version = "termsweeper 0.03";
 const char *argp_program_bug_address = "<erik_andersson007@hotmail.com>";
 
 
 /* Program documentation. */
 static char doc[] = "termsweeper - a simple minesweeper clone in the\
- terminal.";
+terminal.";
 
 /* The options we understand. */
 static struct argp_option options[] = {
-  {"width",   'w', "WIDTH",  0,
-   "Change number of columns in the game to WIDTH." },
-  {"height",   'h', "HEIGHT",  0,
-   "Change number of rows in the game to HEIGHT." },
-  {"mines",   'm', "MINES",  0,
-   "Set the number of mines to put on the board to MINES. MINES has to be\
- larger than 0 and smaller than HEIGHT*WIDTH." },
-  { 0 }
+	{"width",   'w', "WIDTH",  0,
+	"Change number of columns in the game to WIDTH." },
+	{"height",   'h', "HEIGHT",  0,
+	"Change number of rows in the game to HEIGHT." },
+	{"mines",   'm', "MINES",  0,
+	"Set the number of mines to put on the board to MINES. MINES has to be\
+	larger than 0 and smaller than HEIGHT*WIDTH." },
+	{ 0 }
 };
 
 /* Used by main to communicate with parse_opt. */
@@ -42,7 +46,8 @@ parse_opt (int key, char *arg, struct argp_state *state) {
 	/* Get the input argument from argp_parse, which we
 	 * know is a pointer to our arguments structure. */
 	struct arguments *arguments = state->input;
-
+	int upper;
+	int lower;
 	switch (key) {
 		case 'w':
 			arguments->width = arg ? atoi (arg) : 10;
@@ -57,9 +62,12 @@ parse_opt (int key, char *arg, struct argp_state *state) {
 			state->next = state->argc;
 			break;
 		case ARGP_KEY_END:
-			if(arguments->mine_count >= arguments->width*arguments->height)
-				argp_error(state, "Number of mine has to be larger than 0 and\
- smaller than the width*height.");
+			lower = 0;
+			upper = arguments->width*arguments->height;
+			if(arguments->mine_count >= upper || arguments->mine_count <= lower) {
+				argp_error(state, "Number of mine has to be larger than 0 and \
+smaller than the width*height.");
+			}
 			break;
 		default:
 			return ARGP_ERR_UNKNOWN;
@@ -74,122 +82,121 @@ int isSolved(int *game, struct arguments args) {
 	int flag_count = 0;
 	for(int i=0; i<args.height; i++) {
 		for(int j=0; j<args.width; j++) {
-			switch(game[i*args.width + j]) {
-				case FLAG_AND_EMPTY:
-				case FLAG_AND_MINE:
-					flag_count++;
-					break;
-				case EMPTY:
-				case MINE:
-				default:
-					break;
-			}
+
 		}
 	}
 	return flag_count;
 }
 
 int nDigits(int integer) {
-  return floor(log10(abs(integer))) + 1;
+	return floor(log10(abs(integer))) + 1;
 }
 
 int nnDigits(int num, ...) {
-  va_list valist;
-  int sum = 0;
-  va_start(valist, num);
+	va_list valist;
+	int sum = 0;
+	va_start(valist, num);
 
-  for(int i=0; i<num; i++) {
-    sum += nDigits(va_arg(valist, int));
-  }
+	for(int i=0; i<num; i++) {
+		sum += nDigits(va_arg(valist, int));
+	}
 
-  return sum;
+	return sum;
 }
 
-int
-main (int argc, char **argv)
+int* initGame(struct arguments args) {
+	int *game;
+	int h = args.height;
+	int w = args.width;
+	int m = args.mine_count;
+	int numValid = h*w;
+	int* validMine = malloc(numValid*sizeof(int));
+	for(int i=0; i<h*w; i++) {
+		validMine[i] = i;
+	}
+	game = malloc(h*w*sizeof(int));
+	for(int i=0; i<h; i++) {
+		for(int j=0; j<w; j++) {
+			game[i*w + j] = EMPTY;
+		}
+	}
+	for(int i=0; i<m; i++) {
+		int pos = rand()%numValid;
+		game[validMine[pos]] = MINE;
+		for(int j=pos; j<numValid; j++) {
+			validMine[j] = validMine[j+1];
+		}
+		numValid--;
+	}
+	free(validMine);
+	return game;
+}
+
+int main (int argc, char **argv)
 {
+	srand(time(NULL));
 	struct arguments arguments;
 
 	/* Default values. */
-	arguments.width = 80;
-	arguments.height = 40;
-	arguments.mine_count = 10;
+	arguments.width = DEFAULT_W;
+	arguments.height = DEFAULT_H;
+	arguments.mine_count = DEFAULT_M;
 
 
 	/* Parse our arguments; every option seen by parse_opt will be
 	 * reflected in arguments. */
 	argp_parse (&argp, argc, argv, 0, 0, &arguments);
-  int row,col;
-  char *msg;
-  char format[] = "width %d, height %d, mines %d, scr rows %d, scr cols %d";
+	int row,col;
+	int *game = initGame(arguments);
+	int *flags = malloc(1*sizeof(int));
+	int numFlags = 0;
+	char *msg;
+	char format[] = "width %d, height %d, mines %d, scr rows %d, scr cols %d";
 	initscr();
-  do {
-    clear();
-    getmaxyx(stdscr,row,col);
-    int len = strlen(format) + nnDigits(5,arguments.width, arguments.height,
-                                        arguments.mine_count, row, col);
-    if(col<arguments.width || row<arguments.height) {
-      len = strlen("size of window too small, ensure %dx%dx") + nnDigits(2,
-            arguments.width,arguments.height);
-      msg = malloc(sizeof(char)*len);
-      snprintf(msg,len,"size of window too small, ensure %dx%d",arguments.width, arguments.height);
-    } else {
-      msg = malloc(sizeof(char)*len);
-      snprintf(msg, len, format, arguments.width, arguments.height,
-               arguments.mine_count, row, col);
-    }
-    int centery = row/2, centerx = col/2;
-    for(int i=0; i<arguments.height; i++) {
-      for(int j=0; j<arguments.width; j++) {
-        if(i==0||i==arguments.height-1||j==0||j==arguments.width-1) {
-          mvaddch(centery-arguments.height/2+i,centerx-arguments.width/2+j,254);
-        }
-      }
-    }
-    mvprintw (centery,centerx-strlen(msg)/2,msg);
-    refresh();
-    free(msg);
-  } while (getch() != 113);
-	endwin();
-
-	// parse flags
-	// ask for input not provided at launch
-	// generate empty matrix of correct size
-	int *game;
-	game = malloc(arguments.height*arguments.width*sizeof(int));
-	for(int i=0; i<arguments.height; i++) {
-		for(int j=0; j<arguments.width; j++) {
-			game[i*arguments.width + j] = FLAG_AND_EMPTY;
-		}
-	}
-	// start game loop
-	while(!isSolved(game, arguments)) {
-		int flag_count = 0;
-		for(int i=0; i<arguments.height; i++) {
-			for(int j=0; j<arguments.width; j++) {
-				switch(game[i*arguments.width + j]) {
-					case EMPTY:
-						printf("0");
-						break;
-					case MINE:
-						printf("*");
-						break;
-					case FLAG_AND_EMPTY:
-						printf("1");
-						break;
-					case FLAG_AND_MINE:
-						printf(".");
-						break;
-					default:
-			      printf("?");
+	do {
+		clear();
+		getmaxyx(stdscr,row,col);
+		int len = strlen(format) + nnDigits(5,arguments.width, arguments.height,
+			arguments.mine_count, row, col), centery = row/2, centerx = col/2;
+		if(col<arguments.width || row<arguments.height) {
+			len = strlen("size of window too small, ensure %dx%dx") + nnDigits(2,
+				arguments.width,arguments.height);
+			msg = malloc(sizeof(char)*len);
+			snprintf(msg,len,"size of window too small, ensure %dx%d",arguments.width, arguments.height);
+			mvprintw (centery,centerx-strlen(msg)/2,msg);
+		} else {
+			msg = malloc(sizeof(char)*len);
+			//snprintf(msg, len, format, arguments.width, arguments.height,
+			//	arguments.mine_count, row, col);
+			for(int i=-1; i<arguments.height+1; i++) {
+				for(int j=-1; j<arguments.width+1; j++) {
+					if(i==-1||i==arguments.height||j==-1||j==arguments.width) {
+						mvaddch(centery-arguments.height/2+i,centerx-arguments.width/2+j,254);
+					} else {
+						switch(game[i*arguments.width + j]) {
+							case EMPTY:
+								mvaddch(centery-arguments.height/2+i,centerx-arguments.width/2+j,46);
+								break;
+							case MINE:
+								mvaddch(centery-arguments.height/2+i,centerx-arguments.width/2+j,42);
+								break;
+							default:
+								break;
+						}
+					}
 				}
 			}
-			printf("\n");
+			for(int i=0; i<numFlags; i++) {
+				int x=flags[i]%arguments.width;
+				int y=flags[i]/arguments.width;
+				mvaddch(centery-arguments.height/2+y,centerx-arguments.width/2+x,49);
+			}
 		}
-		// draw ui
-		// draw matrix
-		// wait for first click and then place the mines
-	}
+		refresh();
+		free(msg);
+	} while (getch() != 113);
+	endwin();
 	free(game);
-  exit (0);
+	free(flags);
+	exit (0);
 }
